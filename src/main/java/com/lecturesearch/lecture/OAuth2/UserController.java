d8e463570b2234fca60ce79fea06583cc1b075a4
DevAndy <creator.youngjin@gmail.com>
2019-11-07 19:50:44 +0900

    Refactor controller and view

--- src/main/java/com/lecturesearch/lecture/OAuth2/UserController.java
+++ src/main/java/com/lecturesearch/lecture/OAuth2/UserController.java
@@ -3,13 +3,14 @@ package com.lecturesearch.lecture.OAuth2;
 import com.lecturesearch.lecture.OAuth2.annotation.SocialUser;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class UserController {
 
     @GetMapping("/login")
     public String login(){
-        return "login";
+        return "form";
     }
 
     @GetMapping(value = "/loginSuccess")
@@ -18,9 +19,10 @@ public class UserController {
         return "redirect:/main";
     }
 
-
-    @GetMapping("/signup")
-    public String signup(){
-        return "signup";
+    @GetMapping("/create")
+    public String create(String userEmail, String userPassword){
+        System.out.println("email : "+userEmail+" password : "+userPassword);
+        return "layout/main";
     }
+
 }
