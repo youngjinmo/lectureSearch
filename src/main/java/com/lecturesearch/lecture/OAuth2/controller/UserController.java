eaab060a8c28ba57e8c46a183b258ca24b89eec2
DevAndy <creator.youngjin@gmail.com>
2019-11-14 13:15:28 +0900

    Fix error and Add check password while signup

--- src/main/java/com/lecturesearch/lecture/OAuth2/controller/UserController.java
+++ src/main/java/com/lecturesearch/lecture/OAuth2/controller/UserController.java
@@ -16,20 +16,14 @@ public class UserController {
 
     @GetMapping(value = "/loginSuccess")
     public String loginComplete(@SocialUser User user) {
-        System.out.println(user.getEmail());
+        System.out.println("email : "+user.getEmail()+" name : "+user.getName());
         return "redirect:/main";
     }
 
     @PostMapping("/create")
     public String create(String userEmail, String userPassword){
         System.out.println("email : "+userEmail+" password : "+userPassword);
-        return "redirect:/welcome";
-    }
-
-    @GetMapping("/welcome")
-    public String welcome(String userEmail){
-        System.out.println("Welcome "+userEmail);
-        return "layout/welcome";
+        return "redirect:/main";
     }
 
 }
