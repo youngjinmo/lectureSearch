43770bd03a874934b3b3c6fd0afbc5702e9cbe3b
giyun <patronymic@naver.com>
2019-11-20 20:26:38 +0900

    Delete unnecessary test files

--- src/test/java/com/lecturesearch/lecture/LectureApplicationTests.java
+++ src/test/java/com/lecturesearch/lecture/LectureApplicationTests.java
@@ -5,12 +5,14 @@ import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class LectureApplicationTests {
 
     @Test
     public void contextLoads() {
+
     }
 
 }
