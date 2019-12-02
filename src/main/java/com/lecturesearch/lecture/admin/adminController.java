676d1b88bdd91572c3a6a3bf7edaa83953a71e81
giyun <patronymic@naver.com>
2019-12-02 15:04:43 +0900

    Add lecture data page for admin and added function to delete data

--- src/main/java/com/lecturesearch/lecture/admin/adminController.java
+++ src/main/java/com/lecturesearch/lecture/admin/adminController.java
@@ -2,6 +2,8 @@ package com.lecturesearch.lecture.admin;
 
 import com.lecturesearch.lecture.OAuth2.domain.User;
 import com.lecturesearch.lecture.OAuth2.service.UserService;
+import com.lecturesearch.lecture.contents.ContentsService;
+import com.lecturesearch.lecture.contents.ContentsVO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -17,11 +19,34 @@ public class adminController {
     @Autowired
     UserService userService;
 
+    @Autowired
+    ContentsService contentsService;
+
     @RequestMapping("/usersData")
     public String showUserData(Model model){
-        List<User> users=new ArrayList<>();
+        List<User> users;
         users=userService.findAll();
         model.addAttribute("users",users);
-        return "admin/basic_table";
+        return "admin/user_table";
     }
+
+    @RequestMapping("/lectureData")
+    public String showLectureDate(Model model){
+        List<ContentsVO> contents;
+        contents=contentsService.findAll();
+        model.addAttribute("contents",contents);
+        return "admin/lectures_table";
+    }
+
+    @RequestMapping("/deleteContent")
+    public String deleteContent(String idx){
+        contentsService.deleteContent(idx);
+        return "redirect:lectureData";
+    }
+    @RequestMapping("/deleteUser")
+    public String deleteUser(String idx){
+        userService.deleteUser(idx);
+        return "redirect:usersData";
+    }
+
 }
