5041004dc7485846725b752310868f4381cf4247
DevAndy <creator.youngjin@gmail.com>
2019-12-05 17:41:44 +0900

    branch 4 fix

--- src/main/java/com/lecturesearch/lecture/OAuth2/controller/UserController.java
+++ src/main/java/com/lecturesearch/lecture/OAuth2/controller/UserController.java
@@ -23,29 +23,29 @@ public class UserController {
         return "form";
     }
 
-    @PostMapping("/login")
-    public String loginComplete(String email, String password){
-        System.out.println("email :" + email + " password : " + password);
-        Optional user = userRepository.findById(email); // email로 user조회
-        User userDB = new User();
-
-        PasswordEncoding passwordEncoding = new PasswordEncoding();
-
-        // user가 존재하지 않는다면,
-        if(user==null){
-            System.out.println("user가 존재하지않음."); //test
-            return "redirect:/login";
-        } else if(passwordEncoding.matches(password, userDB.getPassword())){
-            // email이 존재한다면, 비밀번호와 비교
-            System.out.println("비밀번호 일치"); //test
-            return "redirect:/main";
-            // loginComplete(email, password);
-        } else {
-            // email이 존재하지만, 비밀번호가 일치하지 않는다면,
-            System.out.println("비밀번호 불일치"); //test
-            return "redirect:/login";
-        }
-    }
+//    @PostMapping("/login")
+//    public String loginComplete(String email, String password){
+//        System.out.println("email :" + email + " password : " + password);
+//        Optional user = userRepository.findById(email); // email로 user조회
+//        User userDB = new User();
+//
+//        PasswordEncoding passwordEncoding = new PasswordEncoding();
+//
+//        // user가 존재하지 않는다면,
+//        if(user==null){
+//            System.out.println("user가 존재하지않음."); //test
+//            return "redirect:/login";
+//        } else if(passwordEncoding.matches(password, userDB.getPassword())){
+//            // email이 존재한다면, 비밀번호와 비교
+//            System.out.println("비밀번호 일치"); //test
+//            return "redirect:/main";
+//            // loginComplete(email, password);
+//        } else {
+//            // email이 존재하지만, 비밀번호가 일치하지 않는다면,
+//            System.out.println("비밀번호 불일치"); //test
+//            return "redirect:/login";
+//        }
+//    }
 
     @GetMapping(value = "/loginSuccess")
     public String loginComplete(@SocialUser User user) {
@@ -74,7 +74,7 @@ public class UserController {
     }
 
     // 회원가입시 이메일 중복체크
-    @RequestMapping(value="/emailChk", method = RequestMethod.POST)
+    @RequestMapping(value="/emailChk", method=RequestMethod.POST)
     public @ResponseBody boolean postIdCheck(@RequestParam("email") String email) {
         boolean result = true;
         if (userService.findByEmail(email).isPresent()) {
@@ -82,6 +82,7 @@ public class UserController {
         }
         return result;
     }
+
     @RequestMapping("/changeStatus")
     public String changeStatus(@RequestBody @RequestParam("email") String email){
         User selectedUser = userService.findByEmail(email).get();
